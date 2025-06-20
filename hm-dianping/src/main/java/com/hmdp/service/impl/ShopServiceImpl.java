58a27b1b3ecf22238457c81236bfeb478da74375
angxing99 <59122619+angxing99@users.noreply.github.com>
2025-06-20 16:30:47 +0800

    feat: cache shop list and type with redis

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,13 +1,55 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Override
+    public Result queryById(Long id) {
+
+        String key = CACHE_SHOP_KEY + id;
+
+        // 1. Get cache from redis
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        // 2. if cache exist, return cache
+        if(StrUtil.isNotBlank(shopJson)){
+           Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+
+        // 3. if no cache found, query DB
+        Shop shop = getById(id);
+
+        // 4. if no data found from DB, return error
+
+        if(shop == null){
+            return Result.fail("Shop Not Found");
+        }
+
+
+        // 5. if data found, save to redis and return
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+
+
+        return Result.ok(shop);
+    }
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,13 +1,66 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.SHOP_TYPE_CACHE_KEY;
 
 
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryTypeList() {
+
+        String key = SHOP_TYPE_CACHE_KEY;
+
+        // 1. Check if cache exist in redis
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(key);
+
+        if(StrUtil.isNotBlank(shopTypeJson)){
+            try {
+                // 2. Parse JSON string to List<ShopType>
+                List<ShopType> cacheList =
+                        new ObjectMapper().readValue(shopTypeJson, new TypeReference<List<ShopType>>() {});
+                return Result.ok(cacheList);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        // 3. Cache miss, query from DB
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+
+        if (typeList == null || typeList.isEmpty()) {
+            return Result.fail("No shop types found.");
+        }
+
+
+        try {
+            // 4. Serialize and store in Redis
+            String jsonStr = new ObjectMapper().writeValueAsString(typeList);
+            stringRedisTemplate.opsForValue().set(key, jsonStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 5. Return result
+        return Result.ok(typeList);
+    }
 }
